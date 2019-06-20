62f150920d85f8fa67c1e1ac5644ae49e4ea42d8
Vic Amuso <vic@parablesoft.com>
2019-06-20 14:12:30 -0400

    Basic data model

--- api/app/models/todo_list.rb
+++ api/app/models/todo_list.rb
@@ -1,2 +1,3 @@
 class TodoList < ApplicationRecord
+  has_many :todos
 end
---
+++ api/db/migrate/20190620181125_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :todos do |t|
+      t.string :content
+      t.boolean :completed, default: false
+      t.belongs_to :todo_list, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
