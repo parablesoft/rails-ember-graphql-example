047c1b99dbaaf1aaa07b75d31a7d11d513e86af9
Vic Amuso <vic@parablesoft.com>
2019-06-20 14:13:27 -0400

    Changed migration

--- api/db/migrate/20190620181125_create_todos.rb
+++ api/db/migrate/20190620181125_create_todos.rb
@@ -2,7 +2,6 @@ class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
       t.string :content
-      t.boolean :completed, default: false
       t.belongs_to :todo_list, foreign_key: true
       t.datetime :completed_at
 
