cbe61e8c422e423948745da9b887705c64b9699f
Vic Amuso <vic@parablesoft.com>
2019-06-20 14:13:11 -0400

    Added completed at to the migration

--- api/db/migrate/20190620181125_create_todos.rb
+++ api/db/migrate/20190620181125_create_todos.rb
@@ -4,6 +4,7 @@ class CreateTodos < ActiveRecord::Migration[5.2]
       t.string :content
       t.boolean :completed, default: false
       t.belongs_to :todo_list, foreign_key: true
+      t.datetime :completed_at
 
       t.timestamps
     end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_133452) do
+ActiveRecord::Schema.define(version: 2019_06_20_181125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,15 @@ ActiveRecord::Schema.define(version: 2019_06_20_133452) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "todos", force: :cascade do |t|
+    t.string "content"
+    t.boolean "completed", default: false
+    t.bigint "todo_list_id"
+    t.datetime "completed_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_list_id"], name: "index_todos_on_todo_list_id"
+  end
+
+  add_foreign_key "todos", "todo_lists"
 end
