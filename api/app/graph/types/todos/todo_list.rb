1c62884cd4788d379566b75c61c89853d8b03096
Vic Amuso <vic@parablesoft.com>
2019-06-20 14:11:04 -0400

    Removed graphql stuff

--- api/app/graph/types/todos/todo_list.rb
+++
@@ -1,9 +0,0 @@
-Stardust::GraphQL.define_types do
-
-  object :todo_list do
-    description "Represents a to-do list"
-    field :id, :id, null: false
-    field :name,:string, null: false
-  end
-
-end
